3294ee8e57476c5677bd8cf0d14c64f1ad610abf
ca1tlin22 <gagn0372@algonquinlive.com>
2016-03-15 17:41:54 -0400

    alternate class created, class toggled

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 var $input = $('#to-do-input');
-var $list = $(".list");
+var $list = $('.list');
 
 $('#to-do-list').on('submit', function (e) {
   var $li = $('<li>');
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('click');
 });
